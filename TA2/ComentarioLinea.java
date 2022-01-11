f1656ea7d1483ca5c6f0adba72aee8cf2844da1d
Víctor Castillo <victor.castillo.barranco@gmail.com>
2022-01-11 09:24:32 +0100

    repositorio

---
+++ TA2/ComentarioLinea.java
@@ -0,0 +1,9 @@
+
+public class ComentarioLinea {
+
+	public static void main(String[] args) {
+		// System.out.println("Hola");
+		System.out.println("Adiós");
+		System.out.println("FIN DE PROGRAMA");
+	}
+}
